d69fe3defbbe4060a50c265f4a886dee51eaa77b
marco <marcoranalli22@gmail.com>
2024-06-27 12:55:34 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+console.log("hello");
+console.log("😎this is my first node project");
+
+let myVar: number = 3;
+myVar += 10;
+console.log("myVar = ", myVar);
+
+interface MyInterface {
+    myNumber: number;
+    myString: string;
+}
+
+let myInterface = {
+    myNumber: 10,
+    myString: "string",
+}
+
+console.log('myNumber = ', myInterface.myNumber);
+console.log('myString = ', myInterface.myString);
